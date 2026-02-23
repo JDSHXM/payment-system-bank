d8108d81bcd0bad6e39ada83f3b9540aea5cc4c5
Dilmurod <dilmurod@gmail.com>
2026-02-23 22:35:09 +0500

    Подключено к базе данных

---
+++ src/main/java/com/paynetSystem/paynetSystemBank/PaynetSystemBankApplication.java
@@ -0,0 +1,13 @@
+package com.paynetSystem.paynetSystemBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaynetSystemBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaynetSystemBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paynetSystem/paynetSystemBank/PaynetSystemBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paynetSystem.paynetSystemBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaynetSystemBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
